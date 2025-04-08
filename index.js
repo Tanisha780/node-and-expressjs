b2a2e715bb5b086f6acbc96a9315101dd7353873
Tanisha780 <141040330+Tanisha780@users.noreply.github.com>
2025-04-08 10:56:42 +0530

    http methods

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
 const express = require("express");
-const app= express();
-
-
+const app = express();
 app.get("/",(req,res)=>{
-  console.log("hello ")
-  res.send("hello world")
+  console.log("hieeeeeeeeeeeeee")
+  res.send("<h1>Hello from Home page</h1>")
 })
-app.get("/home",(req,res)=>{
-  res.send("<h1>hello from home page</h1>")
-
+app.get("/login",(req,res)=>{
+  res.send("<h1>hello from login page</h1>")
 })
-app.get("/product",(req,res)=>{
-  res.send("<h1>hello from product page</h1>")
+app.post("/login",(req,res)=>{
+  res.send("post data sending here..............")
 })
-
+app.put("/login",(req,res)=>{
+  res.send("put data sending here.........")
+})
+app.patch("/login",(req,res)=>{
+  res.send("patch data sending here........")
+})
+app.delete("/login",(req,res)=>{
+  res.send("delete data sending here...........")
+})
 app.listen("5000",()=>{
-  console.log("server is started");
+  console.log("Server is started..........");
   
 })
